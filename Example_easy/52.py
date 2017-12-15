03b1ed7d00cad848f3207dfdf65ede347baaacc2
Sooky <wjdtnwhd777@naver.com>
2017-12-15 18:36:58 +0900

    class 생성자

--- Example_easy/52.py
+++ Example_easy/52.py
@@ -4,10 +4,10 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
-class Myclass():
-    def __init__(self):
-        self.var="안녕하세요!"
-        print("Myclass 인스턴스 객체가 생성되었습니다.")
+class Myclass:
+    def __init__(self, txt):
+        self.var = txt
+        print('txt == '+txt)
 
-obj = Myclass()
+obj = Myclass('Hi!')
 print(obj.var)
